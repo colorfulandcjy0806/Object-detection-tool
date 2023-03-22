aa6f9548207c5f6c4ed3a74d9f2e7c489094efb8
Chen Junyang <97775238+colorfulandcjy0806@users.noreply.github.com>
2023-03-23 00:27:04 +0800

    Update compress.py

--- Documentation Tools/compress.py
+++ Documentation Tools/compress.py
@@ -5,18 +5,22 @@ from PIL import Image
 
 def compress(src_path,dst_path,compress_rate=0.5):
     img = Image.open(src_path)
+    img = img.convert("RGB")
     imageSize = os.path.getsize(src_path)
     img_info = imageSize / 1024
     if (img_info) < 800:
-        return img
+        img.save(dst_path)
+        print("O!"+dst_path)
+        return
     w = int(img.size[0]*compress_rate)
     h = int(img.size[1]*compress_rate)
     img_resize = img.resize((w,h))
     img_resize.save(dst_path)
+    print("success"+dst_path)
 
 if __name__ == "__main__":
-    src_dir = 'a'
-    dst_dir = 'c'
+    src_dir = 'unlabeled'
+    dst_dir = 'u1'
     if not os.path.exists(dst_dir):
         os.mkdir(dst_dir)
     for filename in os.listdir(src_dir):
